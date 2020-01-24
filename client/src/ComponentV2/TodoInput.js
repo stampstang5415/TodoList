290addebceee6dc4fdaa56b481ff3f7bef5fe46b
stampstang5415 <stampstang5415@gmail.com>
2020-01-24 15:52:12 +0700

    24/1/2020

--- client/src/ComponentV2/TodoInput.js
+++ client/src/ComponentV2/TodoInput.js
@@ -1,83 +1,134 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import TodoItem from "./TodoItem.js";
-import { Query, Mutation } from "@apollo/react-components";
+import {Mutation} from "@apollo/react-components";
 import gql from "graphql-tag";
 
-const TODOLIST_QUERY = gql`
-  query TodoListQuery {
-    getTodoList {
-      id
-      title
-      completed
-    }
-  }
-`;
 const ADD_TODO = gql`
-  mutation AddTodo($title: String!) {
-    addTodo(title: $title) {
-      id
-      title
-      completed
+    mutation AddTodo($title: String!) {
+        addTodo(title: $title) {
+            id
+            title
+            completed
+        }
+    }
+`;
+
+const TODOLIST_QUERY = gql`
+    query TodoListQuery {
+        getTodoList {
+            id
+            title
+            completed
+        }
+    }
+`;
+const UPDATE_TITLE = gql`
+    mutation EditTitle($id: ID!, $title: String!) {
+        editTitle(id: $id, title: $title) {
+            id
+            title
+            completed
+        }
+    }
+`;
+const UPDATE_STATUS = gql`
+    mutation EditStatus($id: ID!, $completed: Boolean!) {
+        editStatus(id: $id, completed: $completed) {
+            id
+            title
+            completed
+        }
+    }
+`;
+const REMOVE_TODO = gql`
+    mutation EditStatus($id: ID!) {
+        removeTodo(id: $id) {
+            id
+            title
+            completed
+        }
+    }
+`;
+
+export class TodoInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {new_todo: '', todo_list: []};
+
+    this.handleChange = this.handleChange.bind(this);
+    // this.handleEnterPressed = this.handleEnterPressed.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({new_todo: event.target.value});
+  }
+
+  handleEnterPressed = event => {
+    if (event.key === "Enter") {
+      console.log("Submit");
+      event.currentTarget.value = "";
+
     }
   }
-`;
-export class TodoInput extends Component {
+
+  componentDidMount() {
+
+  }
+
   //ส่วน input รับค่า addtodo ส่งไป Mutations
   render() {
-    let input;
-    // const {
-    //   item,
-    //   // items,
-    //   handleChange
-    //   // handleSubmit
-    //   //   handleDelete
-    // } = this.props;
     return (
       <div className="card card-body my-3">
-        <Mutation mutation={ADD_TODO}>
-          {(addTodo, { data }) => (
+        <Mutation
+          mutation={ADD_TODO}
+          update={(cache, {data: {addTodo}}) => {
+            const {getTodoList} = cache.readQuery({query: TODOLIST_QUERY});
+            cache.writeQuery({
+              query: TODOLIST_QUERY,
+              data: {getTodoList: getTodoList.concat([addTodo])},
+            });
+          }}
+        >
+          {addTodo => (
             <form
               onSubmit={e => {
                 e.preventDefault();
-                addTodo({ variables: { title: input.value } });
-                input.value = "";
+                addTodo({variables: {title: this.state.new_todo}});
+
               }}
             >
               <div className="input-group">
                 <div className="input-group-prepend">
                   <div className="input-group-text bg-primary text-white">
-                    <i className="fas fa-book"></i>
+                    <i className="fas fa-book"/>
                   </div>
                 </div>
                 <input
                   type="text"
                   className="form-control inputtext"
-                  // placeholder="add a Todo item"
-                  ref={node => {
-                    input = node;
-                  }}
+                  value={this.state.newtodo}
+                  onChange={this.handleChange}
+                  onKeyDown={this.handleEnterPressed}
+
                 />
+
               </div>
             </form>
           )}
         </Mutation>
-        {/* <button type="submit" className="btn btn-block btn-primary mt-3">
-            add item
-          </button> */}
         {/* เรียกcomponent TodoItem แสดงlistไอเทม */}
-        <Query query={TODOLIST_QUERY}>
-          {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error :(</p>;
-            // console.log(data);
-            // return <h1>test</h1>;
-            return data.getTodoList.map(({ id, title, completed }) => (
-              <div>
-                <TodoItem key={id} title={title} completed={completed} />
-              </div>
-            ));
-          }}
-        </Query>
+
+        <div>
+          <Mutation mutation={REMOVE_TODO} >
+            {removeTodo => (
+          <Mutation mutation={UPDATE_TITLE} >
+            {editTitle => (
+          <Mutation mutation={UPDATE_STATUS} children={editStatus => <TodoItem removeTodo={removeTodo} editTitle={editTitle} editStatus={editStatus}/>} />
+              )}
+            </Mutation>
+            )}
+          </Mutation>
+        </div>
       </div>
     );
   }
--- client/src/ComponentV2/TodoItem.js
+++ client/src/ComponentV2/TodoItem.js
@@ -1,54 +1,120 @@
-import React, { Component } from "react";
-import "./Checkbox.css";
+import React, {Component} from "react";
+import "./TodoItem.css";
+// import "./Checkbox.css";
+import {Query} from "@apollo/react-components";
+import gql from "graphql-tag";
+
+const TODOLIST_QUERY = gql`
+    query TodoListQuery {
+        getTodoList {
+            id
+            title
+            completed
+        }
+    }
+`;
 export class TodoItem extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { Height: 0, Width: 0, elmnt };
-  // }
-  // getsize() {
-  //   this.setState(state => ({
-  //     Height: elmnt.offsetHeight,
-  //     Width: elmnt.offsetWidth
-  //   }));
-  // }
-  componentDidMount() {
-    // this.elmnt = document.getElementById("list")(() => this.getsize(), 1000);
-    // console.log(Height, "px");
-    // console.log(Width, "px");
+  constructor(props) {
+    super(props);
+    this.state = {new_Title: "", show_Display: false};
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    // this.handleChangeStatus = this.handleChangeStatus.bind(this);
+    // this.handleEditTitle = this.handleEditTitle.bind(this);
+    this.myEdit = React.createRef();
+    this.myEditButton = React.createRef();
   }
+
+  handleChangeTitle(event) {
+    this.setState({new_Title: event.target.value});
+  }
+
+  handleEnterPressed = event => {
+    if (event.key === "Enter") {
+      console.log("Submit");
+      event.currentTarget.value = "";
+
+    }
+  }
+  handleEditTitle = event => {
+    if (this.state.show_Display === false) {
+      // event.preventDefault();
+      this.myEditButton.current.style.display = "none";
+      this.myEdit.current.style.display = "";
+      this.setState({show_Display: true});
+      console.log(this.myEdit.current.style.display);
+    } else {
+      // event.preventDefault();
+      this.myEdit.current.style.display = "none";
+      console.log(this.myEdit.current.style.display);
+    }
+  }
+  handleHiddenEdit= event => {
+    event.preventDefault();
+    this.myEdit.current.style.display = "";
+  }
+  componentDidMount() {
+    // this.myEdit.current.style.display = "none";
+  }
+
   render() {
-    const { title } = this.props;
+    const {editStatus,editTitle,removeTodo} = this.props
+
     return (
-      <li id="list" class="list-item">
-        <input type="checkbox" class="hidden-box" id={title} />
-        <label for={title} class="check--label">
-          <span class="check--label-box"></span>
-          <span class="check--label-text">{title}</span>
-          <div className="todo-icon justify-content-end ">
-            <span className="mx-2 text-success">
-              <i className="fas fa-pen" />
-            </span>
-            <span className="mx-2 text-danger">
-              <i className="fas fa-trash" />
-            </span>
-          </div>
-        </label>
-      </li>
-      // แสดง Listitem ของ Todo
-      // <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
-      //   <input type="checkbox" />
-      //   <span class="checkmark"></span>
-      //   {/* <input type="text" /> */}
-      //   <h6>{title}</h6>
-      //   <div className="todo-icon">
-      //     <span className="mx-2 text-success">
-      //       <i className="fas fa-pen" />
-      //     </span>
-      //     <span className="mx-2 text-danger">
-      //       <i className="fas fa-trash" />
-      //     </span>
-      //   </div>
-      // </li>
+      <Query query={TODOLIST_QUERY}>
+        {({loading, error, data}) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error :(</p>;
+          // console.log(data);
+          // return <h1>test</h1>;
+          return data.getTodoList.map(({id, title, completed}) => {
+            const handleUpdateStatus = event => {
+              event.preventDefault();
+              editStatus({variables: {id, completed: event.target.checked}});
+            }
+            const handleUpdateTitle = event => {
+              event.preventDefault();
+              editTitle({variables: {id, title: this.state.new_Title}});
+            }
+            return (
+              <div key={id}>
+                <li id="list" className="list-item">
+                  <input
+                    type="checkbox" className="hidden-box" id={title} checked={completed}
+                    onChange={handleUpdateStatus}
+                  />
+                  <label htmlFor={title} className="check--label">
+                    <span className="check--label-box"/>
+                    <span className="check--label-text">{title}</span>
+
+                        <form
+                          className="inputEdit"
+                          ref={this.myEdit}
+                          onSubmit={handleUpdateTitle}
+                        >
+                          <input
+                            // value={this.state.new_Title}
+                            onChange={this.handleChangeTitle}
+                            onKeyDown={this.handleEnterPressed}
+                          />
+                          <button type="submit" className="mx-2 text-success" onClick={this.handleHiddenEdit} ><i className="fas fa-pen" /></button>
+                        </form>
+                    <div className="todo-icon justify-content-end ">
+              <span className="mx-2 text-success">
+                <i className="fas fa-pen" ref={this.myEditButton} onClick={this.handleEditTitle}/>
+              </span>
+                      <span className="mx-2 text-danger">
+                <i className="fas fa-trash"/>
+              </span>
+                    </div>
+                  </label>
+                </li>
+              </div>
+
+            );
+          })
+            ;
+        }}
+      </Query>
     );
   }
 }
